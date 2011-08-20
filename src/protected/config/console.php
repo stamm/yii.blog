fcaa8920bac1bbe021c843259aa19fa78e18baeb
Rustam Zagirov <rustam@zagirov.name>
2011-08-21 00:25:29 +0400

    Fix config for console

--- src/protected/config/console.php
+++ src/protected/config/console.php
@@ -6,23 +6,17 @@ $aConfig = CMap::mergeArray(
 	CMap::mergeArray(
 		require(dirname(__FILE__).'/main.php'),
 		array(
-			'basePath'=>dirname(__FILE__).DIRECTORY_SEPARATOR.'..',
-			'name'=>'My Console Application',
-			// application components
 			'components'=>array(
-				'db'=>array(
-					'connectionString' => 'sqlite:'.dirname(__FILE__).'/../data/testdrive.db',
+				'log'=>array(
+					'class'=>'CLogRouter',
+					 'routes'=>array(
+						array(
+							'class'=>'CFileLogRoute',
+							'levels'=>'error, warning, trace, info, profile',
+							'logFile'=>'console.log',
+						),
+					),
 				),
-				// uncomment the following to use a MySQL database
-				/*
-				'db'=>array(
-					'connectionString' => 'mysql:host=localhost;dbname=testdrive',
-					'emulatePrepare' => true,
-					'username' => 'root',
-					'password' => '',
-					'charset' => 'utf8',
-				),
-				*/
 			),
 		)
 	),
