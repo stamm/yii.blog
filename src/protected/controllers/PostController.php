1184fbb5db59ee4ebcf031ea81a948455b81ae1a
Rustam Zagirov <rustam@zagirov.name>
2011-08-23 12:33:28 +0400

    Permission for Post controller

--- src/protected/controllers/PostController.php
+++ src/protected/controllers/PostController.php
@@ -30,13 +30,9 @@ class PostController extends Controller
 				'actions'=>array('index','view'),
 				'users'=>array('*'),
 			),
-			array('allow', // allow authenticated user to perform 'create' and 'update' actions
-				'actions'=>array('create','update'),
-				'users'=>array('@'),
-			),
-			array('allow', // allow admin user to perform 'admin' and 'delete' actions
-				'actions'=>array('admin','delete'),
-				'users'=>array('admin'),
+			array('allow', // allow admin user
+				'actions'=>array('create','update', 'admin','delete'),
+				'roles'=>array('admin'),
 			),
 			array('deny',  // deny all users
 				'users'=>array('*'),
